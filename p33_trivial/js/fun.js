84e37aaf166e4037e84611ad1895ca83055ded1f
MARCELA-3 <150240036+MARCELA-3@users.noreply.github.com>
2023-12-17 23:34:09 +0100

    Add files via upload

---
+++ p33_trivial/js/fun.js
@@ -0,0 +1,19 @@
+if (preguntas.evalue == 0){
+    resultSlot.inner
+
+}
+
+//1.Usuario elige una opcion
+    //Capturo raio buttons
+
+
+//2.Usuario pulsa el boton enviar respuesta
+    //Capturo boton enviar respuesta 
+    //Evento click
+    //extraigo valor del radio button elegido
+    //if : Si el valor elegido es igual al que yo defino como correcto pinto RESULT OK
+    //ELSE : Si no pinto ERROR
+
+//3.Se pinta si es correcta o no
+    //Capturar slot para resultado
+    //Pintar resultado dependiendo del tipo de valor elegido
